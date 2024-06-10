1a60edbad525cce3d730d5ead4bb9a746d32b0b0
aleksanderbl29 <73799306+aleksanderbl29@users.noreply.github.com>
2024-06-10 15:52:54 +0200

    add number formatting function MVP

--- R/nm_fm.R
+++ R/nm_fm.R
@@ -1,3 +1,16 @@
-nm_fm <- function(x) {
-  format(round(x, 2), big.mark = ".", decimal.mark = ",", scientific = FALSE)
+#' Format a number for presentation
+#'
+#' @param x An integer/double value
+#' @param big.mark The character that divides numbers in thousands
+#' @param decimal.mark The decimal separator
+#'
+#' @return A number formatted correctly with two (default) decimals, a comma as the decimal separator and a period as the thousand-separator.
+#' @export
+#'
+#' @examples
+#' x <- pi
+#' nm_fm(x)
+#'
+nm_fm <- function(x, big.mark = ".", decimal.mark = ",") {
+  format(round(x, 2), big.mark = big.mark, decimal.mark = decimal.mark, scientific = FALSE)
 }
