02a2e4510a0647d6d904ed8f793cdd5e0d604987
aleksanderbl29 <73799306+aleksanderbl29@users.noreply.github.com>
2024-06-27 19:38:05 +0200

    Add tests for types and input options

--- R/format_num.R
+++ R/format_num.R
@@ -4,7 +4,7 @@
 #' @param big.mark The character that divides numbers in thousands
 #' @param decimal.mark The decimal separator
 #'
-#' @return A number formatted correctly with two (default) decimals, a comma as the decimal separator and a period as the thousand-separator.
+#' @return A number formatted correctly with two (default) decimals, a comma as the decimal separator and a period as the thousand-separator. The type is *characther*.
 #' @export
 #'
 #' @examples
@@ -12,6 +12,18 @@
 #' format_num(x)
 #'
 format_num <- function(x, decimals = 2, rounding = TRUE, big.mark = ".", decimal.mark = ",") {
+
+  if (is.numeric(x)) {
+    # No error
+  } else if (is.character(x)) {
+    stop("Input x is of type character")
+  } else if (is.data.frame(x)) {
+    stop("Input x is a data.frame")
+  }
+
+  if (decimals != 2 & rounding == FALSE) {
+    stop("Cannot round to given amount of decimals when rounding is off. The options are mutually exclusive.")
+  }
 
   if (rounding == TRUE) {
     x <- round(x, decimals)
--- tests/testthat/test-format_num.R
+++ tests/testthat/test-format_num.R
@@ -1,15 +1,16 @@
 test_that("format_num() formats pi correctly", {
   pi_2_dec <- "3,14"
   pi_4_dec <- "3,1416"
-  pi_4_dec_round_off <- "3,1415"
+  pi_round_off <- "3,141593"
 
   expect_equal(format_num(pi), pi_2_dec)
   expect_equal(format_num(pi, 2), pi_2_dec)
   expect_equal(format_num(pi, 2), format_num(pi))
 
   expect_equal(format_num(pi, 4), pi_4_dec)
+  expect_equal(format_num(pi, rounding = FALSE), pi_round_off)
+
   expect_error(format_num(pi, 4, rounding = FALSE))
-  expect_equal(format_num(pi, 4), pi_4_dec_round_off)
 
 })
 
@@ -17,9 +18,10 @@ test_that("format_num() throws error when given wrong types", {
 
   double <- 3.1415
   string <- as.character("3,1415")
-
+  df <- data.frame()
 
   expect_error(format_num(string))
+  expect_error(format_num(df))
 
   expect_no_error(format_num(double))
 
