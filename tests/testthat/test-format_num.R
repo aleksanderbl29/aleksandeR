b62ec1454b1806e7ea7671cc23004d7c49482246
aleksanderbl29 <73799306+aleksanderbl29@users.noreply.github.com>
2024-06-27 19:07:21 +0200

    Redo tests for newly named number function

--- tests/testthat/test-format_num.R
+++ tests/testthat/test-format_num.R
@@ -1,3 +1,26 @@
-test_that("nm_fm() formats pi correctly", {
-  expect_equal(nm_fm(pi), "3,14")
+test_that("format_num() formats pi correctly", {
+  pi_2_dec <- "3,14"
+  pi_4_dec <- "3,1416"
+  pi_4_dec_round_off <- "3,1415"
+
+  expect_equal(format_num(pi), pi_2_dec)
+  expect_equal(format_num(pi, 2), pi_2_dec)
+  expect_equal(format_num(pi, 2), format_num(pi))
+
+  expect_equal(format_num(pi, 4), pi_4_dec)
+  expect_error(format_num(pi, 4, rounding = FALSE))
+  expect_equal(format_num(pi, 4), pi_4_dec_round_off)
+
+})
+
+test_that("format_num() throws error when given wrong types", {
+
+  double <- 3.1415
+  string <- as.character("3,1415")
+
+
+  expect_error(format_num(string))
+
+  expect_no_error(format_num(double))
+
 })
