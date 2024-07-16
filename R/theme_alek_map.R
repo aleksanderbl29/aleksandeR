6403a807738e5dc6e9b83ea5822dd74d9ed8b509
aleksanderbl29 <73799306+aleksanderbl29@users.noreply.github.com>
2024-07-16 21:56:44 +0200

    Add color version of theme and add documentation

--- R/theme_alek_map.R
+++ R/theme_alek_map.R
@@ -1,9 +1,12 @@
-#' A theme made for maps from \code{\link[ggplot2]{geom_map}()}.
+#' A theme made for maps from  `ggplot2::geom_map()`.
+#'
+#' This theme is made specifically for maps made with. A color-ready version exists as `theme_map_color()`.
 #'
 #' @inheritParams ggplot2::theme_grey
 #'
+#' @importFrom ggplot2 %+replace% element_blank unit theme theme_bw
+#'
 #' @export
-#' @importFrom ggplot2 %+replace%
 theme_map <- function(base_size = 9, base_family = "") {
   theme_bw(base_size = base_size, base_family = base_family) %+replace%
     theme(
@@ -20,3 +23,33 @@ theme_map <- function(base_size = 9, base_family = "") {
       legend.position = c(0, 0)
     )
 }
+
+#' A color-ready theme made for maps from `ggplot2::geom_map()`.
+#'
+#' This theme is made color-ready specifically for maps.
+#'
+#' @inheritParams ggplot2::theme_grey
+#'
+#' @importFrom ggplot2 %+replace% element_blank unit theme theme_bw
+#'
+#' @export
+theme_map_color <- function(color = "green",
+                              secondary_color = "pink",
+                              background_color = "black",
+                              base_size = 9, base_family = "") {
+  theme_bw(base_size = base_size, base_family = base_family) %+replace%
+    theme(
+      axis.line = element_blank(),
+      axis.text = element_blank(),
+      axis.ticks = element_blank(),
+      axis.title = element_blank(),
+      panel.background = element_blank(),
+      panel.border = element_blank(),
+      panel.grid = element_blank(),
+      panel.spacing = unit(0, "lines"),
+      plot.background = element_blank(),
+      legend.justification = c(0, 0),
+      legend.position = c(0, 0)
+    )
+}
+
