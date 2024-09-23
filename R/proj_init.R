5bc5044f048ce56bdac26508e1a4dc692318d8a1
aleksanderbl29 <73799306+aleksanderbl29@users.noreply.github.com>
2024-09-23 08:33:24 +0200

    add pkg install

--- R/proj_init.R
+++ R/proj_init.R
@@ -4,11 +4,49 @@ project_init <- function(packages = NULL) {
   install.packages(c(
     packages,
     "tidyverse",
-    "marginaleffects",
-    "modelsummary",
+
     "pak"
   ))
   pak::pak("aleksanderbl29/dawaR")
   # pak::pak("aleksanderbl29/aleksandeR")
   pak::pak("gadenbuie/rsthemes")
+}
+
+#' @export
+install_basic_packages <- function(additional_packages = NULL) {
+
+  # Vector of package names
+  packages <- c(
+    "tidyverse",
+    "marginaleffects",
+    "modelsummary",
+    "wesanderson",
+    "here",
+    additional_packages)
+
+  # Loop through the vector, check if installed, and install if missing
+  for (pkg in packages) {
+    if (!suppressPackageStartupMessages(require(pkg, character.only = TRUE))) {
+      cli::cli_alert_info("Installing {.var {pkg}}")
+      install.packages(pkg)
+    } else {
+      cli::cli_alert_success("{.var {pkg}} is already installed")
+    }
+  }
+
+  install.packages(
+    "rsthemes",
+    repos = c(
+      gadenbuie = 'https://gadenbuie.r-universe.dev',
+      getOption("repos"))
+  )
+
+  install.packages(
+    "dawaR",
+    repos = c(
+      "https://aleksanderbl29.r-universe.dev",
+      "https://cloud.r-project.org"
+    )
+  )
+
 }
