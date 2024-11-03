169204a423c105de6347f669d5ced95635c43f93
aleksanderbl29 <73799306+aleksanderbl29@users.noreply.github.com>
2024-11-03 17:42:32 +0100

    Add package bib function

---
+++ R/pkg_bib.R
@@ -0,0 +1,28 @@
+#' @export
+pkg_bib <- function(dependencies = FALSE, filename = "renv_packages.bib") {
+  if (dependencies == FALSE) {
+    pkgs <- simple_bib()
+  } else if (dependencies == TRUE) {
+    pkgs <- dependency_bib()
+  }
+  cli::cli_alert("Creating file pkg_references.bib")
+  print_bib(pkgs = pkgs, filename = filename)
+  cli::cli_alert_info("It includes: ")
+  return(pkgs)
+}
+
+simple_bib <- function() {
+  deps <- renv::dependencies()
+  pkgs <- setdiff(unique(deps$Package), "R")
+  return(pkgs)
+}
+
+dependency_bib <- function() {
+  renvLock <- jsonlite::read_json("renv.lock")
+  pkgs <- names(renvLock$Packages)
+  return(pkgs)
+}
+
+print_bib <- function(pkgs, filename = "renv_packages.bib") {
+  bibtex::write.bib(entry = pkgs, file = "renv_packages.bib", append = FALSE)
+}
